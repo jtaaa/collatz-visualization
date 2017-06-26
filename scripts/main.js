04c11a170d29765cedd0366205d01f89d70b920c
jtaaa <joshuatallum@gmail.com>
2017-06-25 23:40:06 -0400

    Use quadratic Bezier curves

--- scripts/main.js
+++ scripts/main.js
@@ -13,51 +13,98 @@ body.appendChild(canvas);
 
 // get context
 const ctx = canvas.getContext('2d');
-let currentNumber = 0;
-let vec = { x: 4, y: height, mag: 30, dir: Math.PI / 2 }
-let offsetDeg = -0.03;
+let vec = { xa: 4, ya: height, x: 4, y: height, mag: 50, dir: Math.PI / 2 }
+let vec2 = { xa: 4, ya: height, x: 4, y: height, mag: 50, dir: Math.PI / 2.1 }
+let vec3 = { xa: 4, ya: height, x: 4, y: height, mag: 50, dir: Math.PI / 2.1 }
+let vec4 = { xa: 4, ya: height, x: 4, y: height, mag: 40, dir: Math.PI / 1.9 }
+let vec5 = { xa: 4, ya: height, x: 4, y: height, mag: 53, dir: Math.PI / 2.2 }
 
-while (currentNumber < 10000) {
-    console.log(vec);
-    ctx.beginPath();
+function drawCurve(vec, offsetDeg, dampFactor, length, speed) {
     ctx.moveTo(vec.x, vec.y);
-    const DAMPING_FACTOR = 1.03;
-    offsetDeg *= DAMPING_FACTOR;
-    console.log(offsetDeg);
-    let newVec = nextVec(vec, offsetDeg);
-    ctx.lineTo(newVec.x, newVec.y);
-    ctx.lineWidth = 10;
+    vec = nextVec(vec, offsetDeg);
+    let xc = vec.x;
+    let yc = vec.y;
+    let oldxc;
+    let oldyc;
+    let currentNumber = 0;
+    let timoutID = setInterval(() => {
+        console.log(vec);
+        let newVec = nextVec(vec, offsetDeg);
+        oldxc = xc;
+        oldyc = yc;
+        xc = (vec.x + newVec.x) / 2;
+        yc = (vec.y + newVec.y) / 2;
+        offsetDeg *= dampFactor;
 
-    // set line color
-    ctx.strokeStyle = 'white';
-    ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(Math.floor(oldxc), Math.floor(oldyc));
+        ctx.quadraticCurveTo(Math.floor(vec.x), Math.floor(vec.y), Math.floor(xc), Math.floor(yc));
+        ctx.lineWidth = 12;
+        ctx.strokeStyle = 'grey';
+        ctx.stroke();
 
-    vec = newVec;
-    currentNumber += 90;
+        // line color
+        ctx.beginPath();
+        ctx.moveTo(Math.floor(oldxc), Math.floor(oldyc));
+        ctx.quadraticCurveTo(Math.floor(vec.x), Math.floor(vec.y), Math.floor(xc), Math.floor(yc));
+        ctx.lineWidth = 8;
+        ctx.strokeStyle = 'white';
+        ctx.stroke();
+
+        vec = newVec;
+        currentNumber += 1;
+        if (currentNumber > length) {
+            clearInterval(timoutID);
+        }
+    }, speed);
 }
+drawCurve(vec, -0.05, 1.023, 200, 50);
+drawCurve(vec2, -0.05, 1.023, 200, 50);
+drawCurve(vec3, -0.04, 1.023, 200, 50);
+drawCurve(vec4, -0.05, 1.023, 100, 50);
+drawCurve(vec5, -0.05, 1.03, 150, 50);
+
 
 function nextVec(vec, offsetDeg) {
     let theta = null;
     let offsetX = null;
     let offsetY = null;
+    let offsetXA = null;
+    let offsetYA = null;
     if (vec.dir <= Math.PI / 2) {
         // First quadrant
         theta = vec.dir;
+        const CONNECTION_OFFSET = 1 - Math.sin(theta * 2) / 15;
+        console.log(CONNECTION_OFFSET);
+        offsetXA = Math.cos(theta) * vec.mag * CONNECTION_OFFSET;
+        offsetYA = Math.sin(theta) * vec.mag * CONNECTION_OFFSET;
         offsetX = Math.cos(theta) * vec.mag;
         offsetY = Math.sin(theta) * vec.mag;
     } else if (vec.dir <= Math.PI) {
         // Second quadrant
         theta = vec.dir - Math.PI / 2;
+        const CONNECTION_OFFSET = 1 -  Math.sin(theta * 2) / 15;
+        console.log(CONNECTION_OFFSET);
+        offsetXA = -Math.sin(theta) * vec.mag * CONNECTION_OFFSET;
+        offsetYA = Math.cos(theta) * vec.mag * CONNECTION_OFFSET;
         offsetX = -Math.sin(theta) * vec.mag;
         offsetY = Math.cos(theta) * vec.mag;
     } else if (vec.dir <= Math.PI * 3 / 2) {
         // Third quadrant
         theta = vec.dir - Math.PI;
+        const CONNECTION_OFFSET = 1 - Math.sin(theta * 2) / 15;
+        console.log(CONNECTION_OFFSET);
+        offsetXA = -Math.cos(theta) * vec.mag * CONNECTION_OFFSET;
+        offsetYA = -Math.sin(theta) * vec.mag * CONNECTION_OFFSET;
         offsetX = -Math.cos(theta) * vec.mag;
         offsetY = -Math.sin(theta) * vec.mag;
     } else {
         // Fourth quadrant
         theta = vec.dir - Math.PI * 3 / 2;
+        const CONNECTION_OFFSET = 1 - Math.sin(theta * 2) / 15;
+        console.log(CONNECTION_OFFSET);
+        offsetXA = Math.sin(theta) * vec.mag * CONNECTION_OFFSET;
+        offsetYA = -Math.cos(theta) * vec.mag * CONNECTION_OFFSET;
         offsetX = Math.sin(theta) * vec.mag;
         offsetY = -Math.cos(theta) * vec.mag;
     }
@@ -69,6 +116,8 @@ function nextVec(vec, offsetDeg) {
         newDir = Math.PI * 2 + newDir;
     }
     return { 
+        xa: vec.x + offsetXA,
+        ya: vec.y - offsetYA,
         x: vec.x + offsetX,
         y: vec.y - offsetY,
         mag: vec.mag,
