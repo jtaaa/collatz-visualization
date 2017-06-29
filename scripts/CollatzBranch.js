29af5e12c4694cc872a0d81ab6fd7161dd1d8600
jtaaa <joshuatallum@gmail.com>
2017-06-29 01:16:00 -0400

    Switch to using CollatzBranches
     - Now build visual representation from seed number up
     - Each branch automatically creates new branches as required

---
+++ scripts/CollatzBranch.js
@@ -0,0 +1,80 @@
+class CollatzBranch{
+    
+    constructor(context, state, startValues) {
+        this.context = context;
+        this.state = state;
+        this.seed = startValues.seed;
+        this.currentNum = this.seed;
+        this.prevAngle = startValues.prevAngle;
+        this.angleStep = startValues.angleStep;
+        this.prevPos = startValues.prevPos;
+        this.lineLength = startValues.lineLength;
+        this.steps = startValues.steps;
+        this.maxNum = startValues.maxNum;
+        this.maxSteps = startValues.maxSteps;
+    }
+
+    draw() {
+        while (this.currentNum < this.maxNum && this.steps < this.maxSteps) {
+            let newPos = this.getNewPos();
+
+            // draw the outline ===============================================
+            this.context.beginPath();
+            this.context.moveTo(this.prevPos.x, this.prevPos.y);
+            this.context.lineTo(newPos.x, newPos.y);
+            
+            this.context.lineWidth = 18;
+            let outlineColor = new Color();
+            outlineColor.setColorRgb(140, 140, 140);
+            this.context.strokeStyle = outlineColor.getHex();
+            
+            this.context.stroke(); // finish drawing outline ==================
+
+            // draw the actual line ===========================================
+            this.context.beginPath();
+            this.context.moveTo(this.prevPos.x, this.prevPos.y);
+            this.context.lineTo(newPos.x, newPos.y);
+            
+            this.context.lineWidth = 14;
+            let color = new Color();
+            color.setColorRgb(255, 255, 255);
+            this.context.strokeStyle = color.getHex();
+            
+            this.context.stroke(); // finish drawing line =====================
+            
+            this.prevPos = newPos;
+            
+            console.log(this.currentNum);
+            if (this.currentNum % 6 === 4) {
+                let newBranch = new CollatzBranch(this.context, this.state, {
+                    seed: (this.currentNum - 1) / 3,
+                    prevAngle: this.prevAngle,
+                    angleStep: this.angleStep,
+                    prevPos: this.prevPos,
+                    lineLength: this.lineLength,
+                    steps: this.steps,
+                    maxNum: this.maxNum,
+                    maxSteps: this.maxSteps
+                });
+                newBranch.draw();
+            }
+            this.currentNum *= 2;
+            this.steps += 1;
+        }
+        console.log('Seed:', this.seed, 'Steps:', this.steps);
+    }
+
+    getNewPos() {
+        let angle = this.prevAngle;
+        if (this.currentNum % 2 === 1) {
+            angle += -this.angleStep - 0.1;
+            this.leftTurns += 1;
+        } else {
+            angle += this.angleStep;
+        }
+        let x = this.prevPos.x + this.lineLength * Math.cos(angle);
+        let y = this.prevPos.y + this.lineLength * Math.sin(angle);
+        this.prevAngle = angle;
+        return { x, y };
+    }
+}
--- scripts/SceneManager.js
+++ scripts/SceneManager.js
@@ -21,13 +21,29 @@ function SceneManager(canvas, tree) {
         state.height = height;
     }
     this.onWindowResize();
-
-    const points = 2;
-
-    const viz = new CollatzViz(context, state, points);
+    
+    const branches = [];
+    branches.push(new CollatzBranch(context, state, {
+        //     this.seed = startValues.seed;
+        // this.currentNum = this.seed;
+        // this.prevAngle = startValues.prevAngle;
+        // this.angleStep = startValues.angleStep;
+        // this.prevPos = startValues.prevPos;
+        // this.lineLength = startValues.lineLength;
+        // this.steps = startValues.steps;
+        seed: 8,
+        prevAngle: Math.PI * 1.02,
+        angleStep: Math.PI / 13,
+        prevPos: { x: 600, y: state.height },
+        lineLength: 30,
+        steps: 0,
+        maxNum: 100000,
+        maxSteps: 40
+    }));
 
     this.update = function () {
-
-        viz.draw();
+        for (let branch of branches) {
+            branch.draw();
+        }
     }
 }
