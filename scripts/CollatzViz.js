0fb6799c54af716f31c6eebfb06edba9d36bc420
jtaaa <joshuatallum@gmail.com>
2017-06-29 01:11:04 -0400

    Draw line from seed using Collatz function

--- scripts/CollatzViz.js
+++ scripts/CollatzViz.js
@@ -1,27 +1,40 @@
 function CollatzViz(context, state, seed) {
-    
     this.currentNum = seed;
+    this.steps = 0;
     this.prevAngle = Math.PI * 3 / 2;
-    this.angleStep = Math.PI / 10;
-    this.prevPos = { x: 500, y: 800 };
-    this.lineLength = 50;
+    this.angleStep = Math.PI / 40;
+    this.prevPos = { x: 10, y: state.height };
+    this.lineLength = 30;
+
+    this.leftTurns = 0;
 
     this.draw = function() {
-        while (this.currentNum < 100) {
+        while (this.currentNum !== 1 && this.steps < 50) {
             context.beginPath();
             context.moveTo(this.prevPos.x, this.prevPos.y);
             let newPos = this.getNewPos();
             context.lineTo(newPos.x, newPos.y);
             this.prevPos = newPos;
+            context.lineWidth = getRandom(6, 15);
+            let color = new Color();
+            color.setColorRgb(this.currentNum % 255, 70, 70);
+            context.strokeStyle = color.getHex();
             context.stroke();
-            this.currentNum += 1;
+            if (this.currentNum % 2 === 1) {
+                this.currentNum = this.currentNum * 3 + 1;
+            } else {
+                this.currentNum /= 2;
+            }
+            this.steps += 1;
         }
+        console.log('Seed:', seed, 'Steps:', this.steps, 'Left turns:', this.leftTurns);
     }
 
     this.getNewPos = function() {
         let angle = this.prevAngle;
-        if (this.currentNum % 8 > 5) {
+        if (this.currentNum % 2 === 1) {
             angle += -this.angleStep;
+            this.leftTurns += 1;
         } else {
             angle += this.angleStep;
         }
