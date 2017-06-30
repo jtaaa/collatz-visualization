d7d5d934228e27fd005e0ba95ffe24294235e930
joshuatallum@gmail.com <joshuatallum@gmail.com>
2017-06-30 17:39:24 -0400

    Remove unnecessary comments

--- scripts/SceneManager.js
+++ scripts/SceneManager.js
@@ -24,13 +24,6 @@ function SceneManager(canvas, tree) {
 
     this.update = function () {
         const branch = new CollatzBranch(context, state, {
-            //     this.seed = startValues.seed;
-            // this.currentNum = this.seed;
-            // this.prevAngle = startValues.prevAngle;
-            // this.angleStep = startValues.angleStep;
-            // this.prevPos = startValues.prevPos;
-            // this.lineLength = startValues.lineLength;
-            // this.steps = startValues.steps;
             seed: 8,
             prevAngle: Math.PI * 1.02,
             angleStep: Math.PI / 13,
