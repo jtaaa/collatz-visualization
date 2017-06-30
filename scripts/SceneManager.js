09c41130f04d426f415d35e84f9157a5979af629
joshuatallum@gmail.com <joshuatallum@gmail.com>
2017-06-30 17:38:04 -0400

    Clean up the initial branch creation

--- scripts/SceneManager.js
+++ scripts/SceneManager.js
@@ -21,29 +21,25 @@ function SceneManager(canvas, tree) {
         state.height = height;
     }
     this.onWindowResize();
-    
-    const branches = [];
-    branches.push(new CollatzBranch(context, state, {
-        //     this.seed = startValues.seed;
-        // this.currentNum = this.seed;
-        // this.prevAngle = startValues.prevAngle;
-        // this.angleStep = startValues.angleStep;
-        // this.prevPos = startValues.prevPos;
-        // this.lineLength = startValues.lineLength;
-        // this.steps = startValues.steps;
-        seed: 8,
-        prevAngle: Math.PI * 1.02,
-        angleStep: Math.PI / 13,
-        prevPos: { x: 600, y: state.height },
-        lineLength: 30,
-        steps: 0,
-        maxNum: 100000,
-        maxSteps: 40
-    }));
 
     this.update = function () {
-        for (let branch of branches) {
-            branch.draw();
-        }
+        const branch = new CollatzBranch(context, state, {
+            //     this.seed = startValues.seed;
+            // this.currentNum = this.seed;
+            // this.prevAngle = startValues.prevAngle;
+            // this.angleStep = startValues.angleStep;
+            // this.prevPos = startValues.prevPos;
+            // this.lineLength = startValues.lineLength;
+            // this.steps = startValues.steps;
+            seed: 8,
+            prevAngle: Math.PI * 1.02,
+            angleStep: Math.PI / 13,
+            prevPos: { x: 600, y: state.height },
+            lineLength: 30,
+            steps: 0,
+            maxNum: 100000,
+            maxSteps: 40
+        });
+        branch.draw();
     }
 }
